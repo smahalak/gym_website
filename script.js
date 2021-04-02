c09ba85e054a9d1135a96be16ffb5412e13a5484
Steven <Mahalak.steve@outlook.com>
2021-04-02 18:32:11 -0400

    added gsap animations

--- script.js
+++ script.js
@@ -11,3 +11,57 @@ const mobileMenu = () => {
 }
 
 menu.addEventListener('click', mobileMenu)
+
+// gsap
+
+gsap.registerPlugin(ScrollTrigger)
+
+gsap.from('.gsap-home', {
+    duration: 1.5,
+    opacity: 0,
+    x: -150,
+    stagger: 0.3,
+})
+
+gsap.from('.gsap-services', {
+    scrollTrigger: '.gsap-services',
+    duration: 0.5,
+    opacity: 1,
+    x: -150,
+    stagger: 0.12,
+})
+
+gsap.from('.gsap-services-img', {
+    scrollTrigger: '.gsap-services-img',
+    duration: 1.3,
+    opacity: 0,
+    x: -1000,
+})
+
+
+gsap.from('.gsap-membership', {
+    scrollTrigger: '.gsap-membership',
+    duration: 0.5,
+    opacity: 0,
+    y: -150,
+    stagger: 0.9,
+    delay: 1.5,
+})
+
+gsap.from('.gsap-card', {
+    scrollTrigger: '.gsap-card',
+    duration: 0.5,
+    opacity: 0,
+    y: -150,
+    stagger: 0.4,
+    delay: 1,
+})
+
+gsap.from('.gsap-staff', {
+    scrollTrigger: '.gsap-staff',
+    duration: 0.9,
+    opacity: 0,
+    y: -150,
+    stagger: 0.4,
+    delay: .7,
+})
